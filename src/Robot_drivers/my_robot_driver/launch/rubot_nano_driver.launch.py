b54f4d04b2f2fbc9568be20d5bbb4811e0e1f2f9
Dr. Manel Puig i Vidal <36498743+manelpuig@users.noreply.github.com>
2025-05-19 14:29:43 +0200

    xml to py

---
+++ src/Robot_drivers/my_robot_driver/launch/rubot_nano_driver.launch.py
@@ -0,0 +1,29 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        DeclareLaunchArgument('use_sim_time', default_value='False'),
+        DeclareLaunchArgument('robot_name', default_value='fastbot'),
+        DeclareLaunchArgument('serial_port', default_value='/dev/arduino_nano'),
+        DeclareLaunchArgument('baud_rate', default_value='57600'),
+        DeclareLaunchArgument('loop_rate', default_value='30'),
+        DeclareLaunchArgument('encoder_cpr', default_value='2550'),
+
+        Node(
+            package='my_robot_driver',
+            executable='rubot_nano_driver_exec',
+            name='rubot_nano_driver',
+            output='screen',
+            parameters=[{
+                'robot_name': LaunchConfiguration('robot_name'),
+                'serial_port': LaunchConfiguration('serial_port'),
+                'baud_rate': LaunchConfiguration('baud_rate'),
+                'loop_rate': LaunchConfiguration('loop_rate'),
+                'encoder_cpr': LaunchConfiguration('encoder_cpr'),
+                'use_sim_time': LaunchConfiguration('use_sim_time'),
+            }]
+        )
+    ])
