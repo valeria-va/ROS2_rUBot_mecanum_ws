44bd82768edf9d61042167a5f5fcbf2db6c13f2a
valeria-va <valeriavallebueno@gmail.com>
2025-12-19 09:57:21 +0100

    Repeated last experiment

--- src/my_robot_co2map/scripts/plot_co2map_channel.py
+++ src/my_robot_co2map/scripts/plot_co2map_channel.py
@@ -9,7 +9,7 @@ import yaml
 # -----------------------------
 # CONFIGURATION
 # -----------------------------
-csv_path = Path("/home/valeria/Desktop/ROS2_rUBot_mecanum_ws/src/my_robot_co2map/ens160_logs/sensor_log_20251218_165354.csv")
+csv_path = Path("/home/valeria/Desktop/ROS2_rUBot_mecanum_ws/src/my_robot_co2map/ens160_logs/sensor_log_20251219_093522.csv")
 yaml_path = Path("/home/valeria/Desktop/ROS2_rUBot_mecanum_ws/src/Navigation_Projects/my_robot_navigation2/map/rajoles.yaml")
 
 channel_to_plot = 5
